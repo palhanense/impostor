f07c89e37af8d29752e0af357c96899b286f67cf
palhanense <palhanense@gmail.com>
2025-12-25 20:21:10 -0300

    Fix MP Idempotency Key

--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 // Configure MP
 const client = new MercadoPagoConfig({
     accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN || '',
-    options: { timeout: 5000, idempotencyKey: 'impstr-pay-sandbox' }
+    options: { timeout: 5000 }
 });
 const payment = new Payment(client);
 
